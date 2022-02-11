207dd53aaa50e3a97ce2c909b4bc3b499a5527ea
bneveret <eve20003@byui.edu>
2022-02-10 19:03:53 -0700

    fixed comments

--- game/card_pull.py
+++ game/card_pull.py
@@ -20,10 +20,20 @@ class Card:
         self.user_guess = ""
     
     def draw_new_card(self):
+        """Selects a new card and sets the current card.
+            
+            Args:
+                self (Card): An instance of Card.
+            """
         self.current_card = self.next_card
         self.next_card = random.randint(1, 13)
 
     def get_user_input(self):
+        """gets the users guess of higher or lower.
+            
+            Args:
+                self (Card): An instance of Card.
+            """
         self.user_guess = input("Higher or lower? [h/l] ").lower()
         while self.user_guess != "h" and self.user_guess != "l":
             print("\nPlease Type h for Higher or l for Lower")
@@ -31,7 +41,11 @@ class Card:
         return self.user_guess
             
     def guess(self):
-        """ if user's guesses is correct, than 100 point are won, Otherwise, they lose 75 points"""
+        """ if user's guesses is correct, than 100 point are won, Otherwise, they lose 75 points
+        
+            Args:
+                self (Card): An instance of Card.
+            """
         if self.user_guess.lower() == "l" and self.current_card > self.next_card:
             self.points = 100
 
